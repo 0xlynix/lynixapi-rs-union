cbf17e1e520e318ef8d0bf543d5e15b74573702a
Lynix <81735347+lynixfur@users.noreply.github.com>
2024-03-31 10:03:34 +0000

    Add auth

--- src/websockets/freakshock.rs
+++ src/websockets/freakshock.rs
@@ -1,27 +1,60 @@
 use actix_web_actors::ws;
-use actix_web::{web, Error, HttpRequest, HttpResponse};
-use actix::{Actor, StreamHandler};
+use actix_web::{http::header, web, HttpRequest, HttpResponse};
+use actix::{Actor, Addr, StreamHandler};
+use actix::AsyncContext;
 
-struct MyWs;
+struct FreakshockWs;
 
-impl Actor for MyWs {
+impl Actor for FreakshockWs {
     type Context = ws::WebsocketContext<Self>;
+
+    fn started(&mut self, ctx: &mut Self::Context) {
+        println!("[Freakshock WS] Connection established");
+        println!("Address: {:?}", ctx.address());
+    }
+
+    fn stopped(&mut self, ctx: &mut Self::Context) {
+        println!("[Freakshock WS] Connection stopped");
+    }
 }
 
 /// Handler for ws::Message message
-impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for MyWs {
+impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for FreakshockWs {
     fn handle(&mut self, msg: Result<ws::Message, ws::ProtocolError>, ctx: &mut Self::Context) {
         match msg {
             Ok(ws::Message::Ping(msg)) => ctx.pong(&msg),
             Ok(ws::Message::Text(text)) => ctx.text(text),
             Ok(ws::Message::Binary(bin)) => ctx.binary(bin),
+            Ok(ws::Message::Close(reason)) => {
+                // Print message that the connection is closed with IP address of client
+                println!("[Freakshock WS] Connection closed");
+                ctx.close(reason);
+            }
             _ => (),
         }
     }
 }
 
-pub async fn freakshock_ws(req: HttpRequest, stream: web::Payload) -> Result<HttpResponse, Error> {
-    let resp = ws::start(MyWs {}, &req, stream);
-    //println!("{:?}", resp);
-    resp
+pub async fn freakshock_ws(req: HttpRequest, stream: web::Payload) -> Result<HttpResponse, actix_web::Error> {
+    // Extract the Authorization header
+    let auth_header = match req.headers().get(header::AUTHORIZATION) {
+        Some(value) => value,
+        None => return Err(actix_web::error::ErrorUnauthorized("Unauthorized")),
+    };
+
+    println!("[Freakshock WS] Authentication in progress...");
+
+    // Check if it's a bearer token and if the token value is "FREAKHOUND"
+    if let Ok(auth_str) = auth_header.to_str() {
+        if auth_str != "Bearer FREAKHOUND" {
+            // Print authentication error
+            println!("[Freakshock WS] Authentication error");
+            return Err(actix_web::error::ErrorUnauthorized("Unauthorized"));
+        }
+    } else {
+        return Err(actix_web::error::ErrorBadRequest("Bad Request"));
+    }
+
+    let resp = ws::start(FreakshockWs, &req, stream);
+    resp.map_err(actix_web::Error::from)
 }
